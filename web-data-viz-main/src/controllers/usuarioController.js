14a4e1a032edab9dbf2105730914a434e4b44320
leh-sptech <leticia.bastos@sptech.school>
2026-05-07 20:33:50 -0300

    realizado a pagina do usuario

--- web-data-viz-main/src/controllers/usuarioController.js
+++ web-data-viz-main/src/controllers/usuarioController.js
@@ -51,6 +51,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var telefone = req.body.telefoneServer;
 
 
     // Faça as validações dos valores
@@ -60,10 +61,13 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else {
+    } else if (telefone == undefined) {
+        res.status(400).send("Seu telefone está undefined!");
+    } 
+    else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, telefone)
             .then(
                 function (resultado) {
                     res.json(resultado);
